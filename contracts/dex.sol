951ddc9a1f171c94dc02b429f7d991f6c5b3c516
saxon <1@1.com>
2022-06-16 15:53:49 +0430

    Create dex

---
+++ contracts/dex.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.8;
+pragma experimental ABIEncoderV2;
+
+contract Dex is Wallet{
+    
+    enum Side{
+        BUY,
+        SELL,
+    }
+
+    struct Order {
+        uint id;
+        address trader;
+        bool buyOrder;
+        bytes32 ticker;
+        uint amount;
+        uint price;
+    }
+
+    mapping(bytes32 => mapping(uint => Order[]));
+
+    function getOrderBook(bytes32 ticker, Side side) view public returns(Order[] memory) {
+        return orderBook[ticker][uint(side)]
+    }
+
+    function createLimitOrder(){
+        
+    }
+}
+
